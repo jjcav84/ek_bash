d79d98ae6dfcc9620f7fa87aecee101ce01ceeed
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-13 02:45:50 -0600

    script to convert uppercase letters in a string variable to lowercase but it doesn't work for me

---
+++ section_3/vid_6/script_5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+var=DSLCoNnEctioN #declare string composed of uppercase and lowercase letters
+
+echo ${var,,} #change all characters in the string from uppercase to lowercase using a double comma; currently doesn't work on my machine so feel free to help fix this
