c8993e53fead432d3f28a43f3494c3b8fe3db8da
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-13 01:03:35 -0600

    variables and binary to decimal conversion

---
+++ section_3/vid_5/script_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name=tea
+
+echo "The word $name contains ${#name} chars" #this will give you the number of characters in the word of the variable name which is tea which is 3
+echo $(( 2#111 )) #this is a binary representation of number 7 in order to get the decimal representation of number 7
+
