ad035bd475791a369d0b7d0efb130536f1aed3e2
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-13 02:09:09 -0600

    for loop that prints the names of colors defined by a colors variable which is a string of color names  delinated with spaces

---
+++ section_3/vid_6/script_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+colors="red black white" #define variable colors which is a string of color names delineated by spaces
+
+for col in $colors
+do
+        echo $col
+done #for loop, col is a variable to which values are assigned in the argument list which is declined by the variable colors which is a string
