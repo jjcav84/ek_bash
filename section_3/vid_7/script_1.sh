d26807c951c0c541ef636a504e3fbd77a1ff3445
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-13 02:54:38 -0600

    use escape or blackslash character

---
+++ section_3/vid_7/script_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# The escape character is also known as backslash and for example if backslash is placed before a character it tells the interpreter to interpret the character before the backslash literally; useful for including double quotes in your output
+
+echo "\"Linux is Awesome\""
