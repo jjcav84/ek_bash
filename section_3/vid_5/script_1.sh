18b7e888ef4be73ac825219914e123effb97e63c
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-12 23:23:47 -0600

    quotes and the echo command and how they interact with the comment hash mark symbol

---
+++ section_3/vid_5/script_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "tralalalalala # blahblahb"  # the hash mark inside of a quotation is printed it so take care
+
+echo traalalala  # blalalala this is what i was warning about the hash mark inside of a quotation is ignored but outside of a quotation is interpreted by the shell as a comment
+
+
