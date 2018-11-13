0c9d6df83cd03136c3533789fd4002635828fd1c
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-11-13 06:37:17 -0600

    if conditional using not equal test

---
+++ section_3/vid_8/script_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+var=10
+
+if [ "$var" != 0 ] #conditional test if var is not equal to zero
+then
+        echo "NOT"
+else
+        echo "YES" #if is not equal to 0 print out NOT else if equal to 0 then print out YES
+fi
