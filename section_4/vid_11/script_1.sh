75918499f6692e899f7045a0d2d82818a82c2124
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-12-12 00:36:25 -0600

    created directory for video 11 and wrote first three scripts

---
+++ section_4/vid_11/script_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+var=10
+
+echo var
+echo $var #this will echo the string var and then the number 10 the name is not the value
+
+
---
+++ section_4/vid_11/script_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+var=10
+
+unset var #this reinitializes the variable to null
+
+echo $var #this will echo nothing
