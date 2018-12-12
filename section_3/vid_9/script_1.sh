ec79a06f1fb7beec7e9366798a14a3fe5ccb140f
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-12-11 19:49:34 -0600

    first script

--- section_3/vid_9/script_1.sh
+++ section_3/vid_9/script_1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#parens are used to store groups of commands and will start a subshell in which the variables inside cannot be read by the script which can be problematic
 
+var=5
+(var=10;)
+
+echo $var #the script will print out 5 due to the parentheses
